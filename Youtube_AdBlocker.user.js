f3d0c7c5574915fee33bfd17f0edeffd45d70c1b
ふぇあ <chitoseame.jp@gmail.com>
2025-08-03 22:06:39 +0900

    Update Youtube_AdBlocker.user.js

--- Youtube_AdBlocker.user.js
+++ Youtube_AdBlocker.user.js
@@ -3,15 +3,9 @@
 // @description  Youtubeの広告を倍速再生する
 // @author       ふぇあ
 // @version      0.0.1
-// @match        *://www.youtube.com/*
+// @match        *://*/*
 // @grant        none
 // @inject-into  content
 // ==/UserScript==
 
-const video=document.querySelector("video");
-function isAdPlaying(){
- return document.querySelector(".ad-showing");
-}
-if(isAdPlaying){
-  alert("広告が検出されました");
-}
+alert(window.location.href);

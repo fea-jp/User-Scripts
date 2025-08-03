0d8aa572c4e8cc8d02372ad62a8686e644187537
ふぇあ <chitoseame.jp@gmail.com>
2025-08-03 21:56:48 +0900

    Update and rename Test.user.js to Youtube_AdBlocker.user.js

---
+++ Youtube_AdBlocker.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Youtube Ad Blocker
+// @description  Youtubeの広告を倍速再生する
+// @author       ふぇあ
+// @version      0.0.1
+// @match        *://*.youtube.com/*
+// @grant        none
+// @inject-into  content
+// ==/UserScript==
+
+const video=document.querySelector("video");
+function isAdPlaying(){
+ return document.querySelector(".ad-showing");
+}
+if(isAdPlaying){
+  alert("広告が検出されました");
+}
